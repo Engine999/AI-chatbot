db2011e204168c8582ffc1e123f618717e21b147
Engine <no8engine@gmail.com>
2025-11-04 10:13:45 +0900

    test code analyze

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -2,15 +2,20 @@ from fastapi import FastAPI
 from google import genai
 from dotenv import load_dotenv
 import os
+import sys
 
 load_dotenv(dotenv_path="C:\\Users\\dbswodyd99\\Desktop\\PTJ\\02.AI-chatbot\\AI-chatbot\\static\\.env")
 api_key = os.getenv("GOOGLE_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+
+user_code = sys.stdin.read()
+
+
 response = client.models.generate_content(
     model="gemini-2.0-flash",
-    contents="Engine을 출력하세요"
+    contents=user_code+"분석해주세요"
 )
 
 print(response.text)
